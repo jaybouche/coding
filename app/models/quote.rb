cb981b3074ca9ad4f200893072968bb3da9c9b85
Jay Boucher <jaybouche@gmail.com>
2019-06-15 18:21:45 +0000

    css add

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord
+  validates :saying, presence: true, length: { maxium: 140, minimum: 3}
+  validates :author, presence: true, length: { maximum: 50, minimum: 3 }
 end
